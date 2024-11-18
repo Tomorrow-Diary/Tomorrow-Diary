aa1983f9d6ff8de281b46407d4926d62fb40519c
rnqhscjf3333 <rnqhscjf3333@naver.com>
2024-11-18 15:54:17 +0900

    Initial commit

---
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/TomorrowdirayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tomorrowdiray_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomorrowdirayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomorrowdirayBackendApplication.class, args);
+	}
+
+}
---
+++ tomorrowdiray_backend/src/test/java/com/ssafy/tomorrowdiray_backend/TomorrowdirayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tomorrowdiray_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowdirayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
