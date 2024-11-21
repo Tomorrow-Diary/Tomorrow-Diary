39ed53709bcf13160a83c243d0f1139ce5e3c2cf
hyeseon <chunghye1998@gmail.com>
2024-11-21 09:49:08 +0900

    feat: logout 구현

--- tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/exception/StatusCode.java
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/exception/StatusCode.java
@@ -12,7 +12,8 @@ public enum StatusCode implements BaseCode{
 	// ----- [User] ----- //
 	SIGNUP_SUCCESS(HttpStatus.OK, "회원가입 성공"),
 	LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
-	REQUIRED_SIGNUP(HttpStatus.ACCEPTED, "회원가입이 필요합니다.");
+	REQUIRED_SIGNUP(HttpStatus.ACCEPTED, "회원가입이 필요합니다."),
+	LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공");
 
 	private final HttpStatus status;
 	private final String message;
