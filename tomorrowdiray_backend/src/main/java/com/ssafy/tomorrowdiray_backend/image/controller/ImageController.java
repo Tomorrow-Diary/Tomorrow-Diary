8379c4b362a27e4aeb6b3600fe05b3308a0cb35d
hyeseon <chunghye1998@gmail.com>
2024-11-25 21:26:01 +0900

    docs: 일기 생성 API, 이미지 업로드 API Swagger 추가

--- tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/image/controller/ImageController.java
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/image/controller/ImageController.java
@@ -3,6 +3,9 @@ package com.ssafy.tomorrowdiray_backend.image.controller;
 import com.ssafy.tomorrowdiray_backend.global.exception.StatusCode;
 import com.ssafy.tomorrowdiray_backend.global.response.BaseApiResponse;
 import com.ssafy.tomorrowdiray_backend.image.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,10 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    @Operation(summary = "이미지 업로드 API", description = "이미지 파일을 S3에 업로드합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "이미지 업로드 성공")
+    })
     @PostMapping("/{diaryId}")
     public ResponseEntity<BaseApiResponse<Void>> createImage(
             @PathVariable Long diaryId,
