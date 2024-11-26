e41b34f36dc72e06d538ee1f8d2946d2a6d53e79
hyeseon <chunghye1998@gmail.com>
2024-11-26 19:24:22 +0900

    fix: cors 해결

--- tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/config/WebConfig.java
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOriginPatterns("https://localhost:5174")
+				.allowedOriginPatterns("http://localhost:5174")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
 				.allowedHeaders("*")
 				.exposedHeaders("Authorization", "Content-Type")
