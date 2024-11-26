acb7b86e76a951693315b2714f19ec258462dda2
hyeseon <chunghye1998@gmail.com>
2024-11-26 20:03:22 +0900

    fix: 인터셉터 주석 해제

--- tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/config/WebConfig.java
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ssafy.tomorrowdiray_backend.global.permission.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,19 +13,19 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthInterceptor authInterceptor;
 
-//	 @Override
-//	 public void addInterceptors(InterceptorRegistry registry) {
-//		 registry.addInterceptor(authInterceptor)
-//				 .addPathPatterns("/**")
-//				 .excludePathPatterns(
-//						 "/api/v1/user/login/**",
-//						 "/api/v1/user/signup",
-//						 "/swagger-resources/**",
-//						 "/swagger-ui/**",
-//						 "/v3/api-docs/**",
-//						 "/callback"
-//				 );
-//	 }
+	 @Override
+	 public void addInterceptors(InterceptorRegistry registry) {
+		 registry.addInterceptor(authInterceptor)
+				 .addPathPatterns("/**")
+				 .excludePathPatterns(
+						 "/api/v1/user/login/**",
+						 "/api/v1/user/signup",
+						 "/swagger-resources/**",
+						 "/swagger-ui/**",
+						 "/v3/api-docs/**",
+						 "/callback"
+				 );
+	 }
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
