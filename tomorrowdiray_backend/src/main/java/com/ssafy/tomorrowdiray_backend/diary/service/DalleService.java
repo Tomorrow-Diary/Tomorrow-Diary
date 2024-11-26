f63df31a3ac74a4d15f5be2228e07994db639965
hyeseon <chunghye1998@gmail.com>
2024-11-26 16:32:45 +0900

    refactor: ai prompt 고도화

--- tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/diary/service/DalleService.java
+++ tomorrowdiray_backend/src/main/java/com/ssafy/tomorrowdiray_backend/diary/service/DalleService.java
@@ -4,6 +4,7 @@ import com.ssafy.tomorrowdiray_backend.diary.dto.response.CreateContentResponse;
 import com.ssafy.tomorrowdiray_backend.global.util.FileUtil;
 import com.ssafy.tomorrowdiray_backend.image.service.S3ImageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.image.Image;
 import org.springframework.ai.image.ImageGeneration;
 import org.springframework.ai.image.ImagePrompt;
@@ -14,8 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DalleService {
@@ -23,32 +24,37 @@ public class DalleService {
     private final OpenAiImageModel openAiImageModel;
     private final S3ImageService s3ImageService;
 
-    public List<String> generateImage(List<CreateContentResponse> diaryContentList, String summary) {
-        String combinedPrompt = diaryContentList.stream()
-                .map(CreateContentResponse::getContent)
-                .map(content -> "- " + content) // 각 프롬프트를 구분하기 위해 "-" 추가
-                .collect(Collectors.joining("\n"))
-                + "\n\nSummary:\n" + summary;
+    public List<String> generateImage(List<CreateContentResponse> diaryContentList) {
+        return diaryContentList.stream()
+                .flatMap(contentResponse -> {
+                    String prompt = "따뜻하고 현실적인 스타일로 \n" +
+                            "- " + contentResponse.getContent() +
+                            "\n 위의 내용을 기반으로 사람이 포함된 2D 그림을 만들어줘.";
 
-        ImageResponse response = openAiImageModel.call(
-                new ImagePrompt(combinedPrompt,
-                        OpenAiImageOptions.builder()
-                                .withQuality("hd")
-                                .withN(5)
-                                .withHeight(1024)
-                                .withWidth(1024)
-                                .withModel("dall-e-2")
-                                .withResponseFormat("b64_json")
-                                .build())
-        );
+                    log.debug("Generated prompt: {}", prompt);
 
-        List<MultipartFile> files = response.getResults().stream()
-                .map(ImageGeneration::getOutput)
-                .map(Image::getB64Json)
-                .map(FileUtil::convertBase64ToMultipartFile)
+                    // 각 프롬프트에 대해 4개의 이미지를 생성
+                    ImageResponse response = openAiImageModel.call(
+                            new ImagePrompt(prompt,
+                                    OpenAiImageOptions.builder()
+                                            .withUser("너는 한국인이야. 종교가 없어.")
+                                            .withN(1)
+                                            .withHeight(1024)
+                                            .withWidth(1024)
+                                            .withModel("dall-e-3")
+                                            .withResponseFormat("b64_json")
+                                            .build())
+                    );
+
+                    List<MultipartFile> files = response.getResults().stream()
+                            .map(ImageGeneration::getOutput)
+                            .map(Image::getB64Json)
+                            .map(FileUtil::convertBase64ToMultipartFile)
+                            .toList();
+
+                    return makeFileToUrl(files).stream();
+                })
                 .toList();
-
-        return makeFileToUrl(files);
     }
 
     private List<String> makeFileToUrl(List<MultipartFile> files) {
